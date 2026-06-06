5c3befdc89fe1c495bdf932030378eb2ad882ff1
sethjd <jordonseth@hotmail.com>
2026-06-06 12:15:36 +0800

    Delete preload.js

--- preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("ArcaneDesktop", {
-  isDesktopWrapper: true,
-  toggleFullscreen: () => ipcRenderer.send("arcane-toggle-fullscreen"),
-  setFullscreen: (enabled) => ipcRenderer.send("arcane-set-fullscreen", Boolean(enabled)),
-  isFullscreen: () => ipcRenderer.invoke("arcane-is-fullscreen"),
-  quitGame: () => ipcRenderer.send("arcane-quit-game")
-});
